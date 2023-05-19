19c531f6f85152e25a1a19f8180eb90020181e50
MrLogan2203 <130476862+MrLogan2203@users.noreply.github.com>
2023-05-19 23:50:57 +0300

    feature: added scraping with depth

--- scrapping/scrap.py
+++ scrapping/scrap.py
@@ -1,26 +1,38 @@
 import requests
-from urllib.parse import urlparse 
+from urllib.parse import urlparse, urljoin 
 import re
 import requests
 from bs4 import BeautifulSoup
 
 links = []
 
-def scrap(url):
+def scrap(url,depth =1):
+        if depth == 0:
+                return
         parsed_url = urlparse(url)
         if not parsed_url.scheme:
                 url = 'https://' + url
                        
         response = requests.get(url)
-        html_content = response.text
+        html_content = response.content
+        #print(html_content)
         
         soup = BeautifulSoup(html_content, "html.parser")
-        for link in soup.find_all('a', attrs ={'href': re.compile("/.*/$")}):
-                link = link.get("href") 
-                if 'http' in link:
+        for link in soup.find_all('a'):
+                try: 
+                        link = link.get("href") 
+                        if link.startswith("http"):
+                                continue
+                        else:
+                                new_url = urljoin(url,link)
+                                if new_url not in links: 
+                                        print(new_url)
+                                        links.append(new_url)
+                                        scrap(new_url,depth -1)
+                                else:
+                                        continue
+                except requests.exceptions.ConnectionError:
                         continue
-                else:
-                        links.append(link)
-        for link in links:
-                print(url+link)
+        #for link in links:
+                #print(url+link)
                 
