00586aefb73ff831b798f67888efe36d8dc37c11
MrLogan2203 <130476862+MrLogan2203@users.noreply.github.com>
2023-05-20 23:12:26 +0300

    fix: fixed bug with depth
    Apparently, the depth system was working all wrong

--- scrapping/scrap.py
+++ scrapping/scrap.py
@@ -1,38 +1,40 @@
 import requests
-from urllib.parse import urlparse, urljoin 
+from urllib.parse import urlparse, urljoin
 import re
 import requests
 from bs4 import BeautifulSoup
 
-links = []
+intlinks = []
+extlinks = []
 
-def scrap(url,depth =1):
-        if depth == 0:
-                return
-        parsed_url = urlparse(url)
-        if not parsed_url.scheme:
-                url = 'https://' + url
-                       
-        response = requests.get(url)
-        html_content = response.content
-        #print(html_content)
-        
-        soup = BeautifulSoup(html_content, "html.parser")
-        for link in soup.find_all('a'):
-                try: 
-                        link = link.get("href") 
-                        if link.startswith("http"):
-                                continue
-                        else:
-                                new_url = urljoin(url,link)
-                                if new_url not in links: 
-                                        print(new_url)
-                                        links.append(new_url)
-                                        scrap(new_url,depth -1)
-                                else:
-                                        continue
-                except requests.exceptions.ConnectionError:
+def scrap(url, depth = 1):
+    parsed_url = urlparse(url)
+    if not parsed_url.scheme:
+        url = 'https://' + url +'/'
+    response = requests.get(url)
+    html_content = response.content
+    soup = BeautifulSoup(html_content, "lxml")
+    for bs_link in soup.find_all('a'):
+        try:
+            new_link = bs_link.get("href")
+            if new_link.startswith("http"):
+                extlinks.append(urljoin(url,new_link))
+            else:
+                if depth >=1:
+                    new_url = urljoin(url, new_link)
+                    if new_link.startswith('/') and new_url not in intlinks:
+                        #print(new_url)
+                        intlinks.append(new_url)
+                        scrap(new_url,depth-1)
+                    else:
                         continue
-        #for link in links:
-                #print(url+link)
-                
+        except AttributeError:
+            continue
+
+def save(url):
+	with open("./scrapping results/internal_"+url.replace(".","_")+".txt","a") as f:
+		for link in intlinks:
+			f.write(link+"\n")
+	with open("./scrapping results/external_"+url.replace(".","_")+".txt","a") as f:
+			for link in extlinks:
+				f.write(link+"\n")
