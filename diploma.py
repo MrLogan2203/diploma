5f6945f05c386e447e547d3ed2f88c3182780669
MrLogan2203 <130476862+MrLogan2203@users.noreply.github.com>
2023-05-21 16:20:33 +0300

    change: changed a main controlling file
    New changes have been applied to the main file

--- diploma.py
+++ diploma.py
@@ -1,11 +1,11 @@
 import argparse
 import colorama
 from colorama import Fore
-from port_scanner import Scan_Port
 from shodan_test import Scan_Inf
 from scrapping.scripts_search import Scan_CGI, Scan_JS
 from scrapping.scrap import scrap,save
 from scrapping.headers_search import Headers_Search
+from port_scanner import run,scan
 
 def main():
         # Create an argument parser
@@ -17,6 +17,8 @@ def main():
         # Add the '-s' argument for handling scrap function
         parser.add_argument('-s', metavar='<int>', type=int, nargs='?',const=1, help='provide an integer value for scrapping depth. default = 1, only provided host is scanning')
         
+        parser.add_argument('-p', metavar='<str>', type=str, nargs='?',const='1-65535', help ='provide the str range of ports to scan e.g. 1-500. Default is 1-65535, mention that it might be time consuming') 
+        
         # Add the '--scripts' argument for handling the headers_searcher functions
         parser.add_argument('--scripts', action='store_true', help='use this argument to search for CGI and JS scripts on a target host')
 
@@ -25,9 +27,15 @@ def main():
 
         # Check if the '-t' argument was provided
         if args.t:
-            Scan_Port(args.t)
             Scan_Inf(args.t)
             Headers_Search(args.t)
+            
+        if args.t and args.p:
+            host = args.t
+            start_port, end_port = map(int,args.p.split('-'))
+            print(Fore.GREEN + f"Scanning for open ports from port {start_port} to {end_port}..."+Fore.WHITE)
+            run(host, scan, start_port, end_port)
+            print(Fore.GREEN + f"Scanning is done!"+Fore.WHITE)
 
         # Check if the '-s' argument was provided
         if args.t and args.s:
